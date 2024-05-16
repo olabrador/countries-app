96682e9ad1e8efa16d1e3ebc235b94c2af192a84
olabrador <labradoruseche@gmail.com>
2024-05-16 13:28:05 -0400

    fix(utils/index): ensure parameters are positive integers

--- amplify/backend/function/getCountryInformation/src/utils/index.js
+++ amplify/backend/function/getCountryInformation/src/utils/index.js
@@ -1,7 +1,7 @@
 function isValidPagination(page, rows) {
   const pageNumber = Number(page);
   const rowsNumber = Number(rows);
-  return Number.isInteger(pageNumber) && Number.isInteger(rowsNumber) && pageNumber < 1 && rowsNumber < 1;
+  return Number.isInteger(pageNumber) && Number.isInteger(rowsNumber) && pageNumber > 0 && rowsNumber > 0;
 }
 
 module.exports = {
