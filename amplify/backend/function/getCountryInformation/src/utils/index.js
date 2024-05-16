1de68edb469382f404fb30ccfeeb47d3fd040a3f
olabrador <labradoruseche@gmail.com>
2024-05-16 12:56:05 -0400

    fix: bad export

--- amplify/backend/function/getCountryInformation/src/utils/index.js
+++ amplify/backend/function/getCountryInformation/src/utils/index.js
@@ -1,5 +1,9 @@
-export function isValidPagination(page, rows) {
+function isValidPagination(page, rows) {
   const pageNumber = Number(page);
   const rowsNumber = Number(rows);
   return Number.isInteger(pageNumber) && Number.isInteger(rowsNumber) && pageNumber < 1 && rowsNumber < 1;
 }
+
+module.exports = {
+  isValidPagination,
+};
